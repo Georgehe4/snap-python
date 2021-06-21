d584d46b332daf9662ca54e0afed540e8d0e6d5a
georgewho96@gmail.com <georgewho96@gmail.com>
2021-06-21 00:25:04 -0700

    Add true random walks

--- swig/linkpred_swig.h
+++ swig/linkpred_swig.h
@@ -1,5 +1,5 @@
 
-// random walk with restars to node JumpNId
+// random walk with restarts to node JumpNId
 template <class PGraph>
 void GetRndWalkRestart(const PGraph& Graph, const double& JumpProb, const int& JumpNId, THash<TInt, TFlt>& RwrNIdH) {
   const double DefVal = 1.0/Graph->GetNodes();
@@ -28,3 +28,44 @@ void GetRndWalkRestart(const PGraph& Graph, const double& JumpProb, const int& J
   }
 }
 
+// True random walks, implementing a recommendation
+// TRnd = TRnd Rnd(0);
+template <class PGraph>
+void GetRndWalkTopN(const PGraph& Graph, const double& JumpProb, THash<TInt, TFlt>& RwsNIdH, THash<TInt, TFlt>& RwrNIdH {
+  // TODO: Push this into the constructor
+  // Seed randomness from clock
+  TRnd Rnd(0);
+  TInt node;
+  TIntV RwsV;
+  RwrNIdH.GetKeyV(RwsV)
+  // Get weighted distribution of starting nodes...
+  // TODO: Actually be weighted, using RwrNIdH
+  int nodeN = Rnd.GetUniDev(RwsNIdH.GetKeys());
+  RwsV[nodeN]
+  
+
+  const double DefVal = 1.0/Graph->GetNodes();
+  RwrNIdH.Clr(false);
+  TIntH NIdOutDegH;
+  for (typename PGraph::TObj::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    RwrNIdH.AddDat(NI.GetId(), DefVal);
+    NIdOutDegH.AddDat(NI.GetId(), NI.GetOutDeg());
+  }
+  THash<TInt, TFlt> RwrNIdH2(Graph->GetNodes());
+  for (int iter = 0; iter < 10; iter++) {
+    double Sum = 0;
+    for (typename PGraph::TObj::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+      double Val = 0;
+      for (int i = 0; i < NI.GetInDeg(); i++) {
+        const int InId = NI.GetInNId(i);
+        Val += (1.0-JumpProb) * 1.0/(NIdOutDegH.GetDat(InId)) * RwrNIdH.GetDat(InId);
+      }
+      if (NI.GetId() == JumpNId) { Val+= JumpProb; }
+      RwrNIdH.AddDat(NI.GetId(), Val);
+      Sum += Val;
+    }
+    for (int i = 0; i < RwrNIdH.Len(); i++) {
+      RwrNIdH[i] /= Sum;
+    }
+  }
+}
